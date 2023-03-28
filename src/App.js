cd4c44b74f6d37d52a17b4b8bf362f24ed035bc3
Ajit Kumar Maurya <kumarmauryaajit@gmail.com>
2023-03-28 18:32:15 +0530

    Added the homepage.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import Home from "./pages/Home/Home";
+// import Home from "./pages/Home/Home";
 import Login from "./pages/login/Login";
-import Profile from "./pages/profile/Profile";
+// import Profile from "./pages/profile/Profile";
 import Register from "./pages/register/Register";
 import {
   BrowserRouter as Router,
@@ -11,24 +11,66 @@ import {
 import { useContext } from "react";
 import { AuthContext } from "./context/AuthContext";
 
-function App() {
-  const { user } = useContext(AuthContext);
+// function App() {
+//   const { user } = useContext(AuthContext);
+//   return (
+//     <Router>
+//       <Switch>
+//         <Route exact path="/">
+//           {user ? <Home /> : <Register />}
+//         </Route>
+//         <Route path="/login">{user ? <Redirect to="/" /> : <Login /> }</Route>
+//         <Route path="/register">
+//           {user ? <Redirect to="/" /> : <Register />}
+//         </Route>
+//         <Route path="/profile/:username">
+//           <Profile />
+//         </Route>
+//       </Switch>
+//     </Router>
+//   );
+// }
+
+// export default App;
+
+import React, { useState, useEffect } from "react";
+import { Navigation } from "./components/navigation";
+import { Header } from "./components/header";
+import { Features } from "./components/features";
+import { About } from "./components/about";
+import { Services } from "./components/services";
+import { Gallery } from "./components/gallery";
+import { Testimonials } from "./components/testimonials";
+import { Team } from "./components/Team";
+import { Contact } from "./components/contact";
+import JsonData from "./data/data.json";
+import SmoothScroll from "smooth-scroll";
+import "./App.css";
+
+export const scroll = new SmoothScroll('a[href*="#"]', {
+  speed: 1000,
+  speedAsDuration: true,
+});
+
+const App = () => {
+  const [landingPageData, setLandingPageData] = useState({});
+  useEffect(() => {
+    setLandingPageData(JsonData);
+  }, []);
+
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          {user ? <Home /> : <Register />}
-        </Route>
-        <Route path="/login">{user ? <Redirect to="/" /> : <Login /> }</Route>
-        <Route path="/register">
-          {user ? <Redirect to="/" /> : <Register />}
-        </Route>
-        <Route path="/profile/:username">
-          <Profile />
-        </Route>
-      </Switch>
-    </Router>
+    <div>
+      <Navigation />
+      <Header data={landingPageData.Header} />
+      <Features data={landingPageData.Features} />
+      <About data={landingPageData.About} />
+      <Services data={landingPageData.Services} />
+      <Gallery data={landingPageData.Gallery} />
+      <Testimonials data={landingPageData.Testimonials} />
+      <Team data={landingPageData.Team} />
+      <Contact data={landingPageData.Contact} />
+    </div>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,15 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import App from './App';
 import { AuthContextProvider } from './context/AuthContext';
-import Home from './pages/Home/Home';
-// import Login from './pages/login/Login';
+// import Home from './pages/Home/Home';
+import Login from './pages/login/Login';
 // import Register from './pages/register/Register';
+
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <AuthContextProvider>
+//     {/* <Home/> */}
+//     <Login/>
+//     {/* <Register/> */}
+//   </AuthContextProvider>
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 ReactDOM.render(
   <React.StrictMode>
-    <AuthContextProvider>
-    <Home/>
-  </AuthContextProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
+serviceWorker.register();
