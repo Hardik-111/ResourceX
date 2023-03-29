38764d7acdd2e6dd321e85685a0c553bc6b6558d
Hardik <ihardik.112@gmail.com>
2023-03-29 22:58:27 +0530

    refactored changes in App.js

--- src/App.js
+++ src/App.js
@@ -33,43 +33,54 @@
 
 // export default App;
 
-import React, { useState, useEffect } from "react";
-import { Navigation } from "./components/Navigation";
-import { Header } from "./components/Header";
-// import { Features } from "./components/features";
-import { About } from "./components/About";
-import { Departments} from "./components/Dept";
-// import { Testimonials } from "./components/testimonials";
-import { Team } from "./components/Team";
-import { Contact } from "./components/Contact";
-import JsonData from "./data/data.json";
-import SmoothScroll from "smooth-scroll";
-import "./App.css";
+// import Home from "./pages/Home/Home";
+// import Login from "./pages/login/Login";
+// import Profile from "./pages/profile/Profile";
+// import Register from "./pages/register/Register";
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+//   Redirect,
+// } from "react-router-dom";
+// import { useContext } from "react";
+// import { AuthContext } from "./context/AuthContext";
 
-export const scroll = new SmoothScroll('a[href*="#"]', {
-  speed: 1000,
-  speedAsDuration: true,
-});
+// function App() {
+//   const { user } = useContext(AuthContext);
+//   return (
+//     <Router>
+//       <Switch>
+//         <Route exact path="/">
+//           {user ? <Home /> : <Register />}
+//         </Route>
+//         <Route path="/login">{user ? <Redirect to="/" /> : <Login /> }</Route>
+//         <Route path="/register">
+//           {user ? <Redirect to="/" /> : <Register />}
+//         </Route>
+//         <Route path="/profile/:username">
+//           <Profile />
+//         </Route>
+//       </Switch>
+//     </Router>
+//   );
+// }
+
+// export default App;
+
+// import React, { useState, useEffect } from "react";
+import "./App.css";
+import Home from "./pages/Home/Home";
 
 const App = () => {
-  const [landingPageData, setLandingPageData] = useState({});
-  useEffect(() => {
-    setLandingPageData(JsonData);
-  }, []);
+
 
   return (
     <div>
-      <Navigation />
-      <Header data={landingPageData.Header} />
-      {/* <Features data={landingPageData.Features} /> */}
-      {/* <Services data={landingPageData.Services} /> */}
-      <Departments data={landingPageData.Departments} />
-      <About data={landingPageData.About} />
-      {/* <Testimonials data={landingPageData.Testimonials} /> */}
-      <Team data={landingPageData.Team} />
-      <Contact data={landingPageData.Contact} />
+      <Home/>
     </div>
   );
 };
 
 export default App;
+
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,47 @@
 import "./home.css"
 
-export default function Home() {
-  return (
-    <>
+import "./home.css"
+import React, { useState, useEffect } from "react";
+// import { Navigation } from "/components/navigation";
 
-    </>
+// import { Features } from "./components/features";
+
+// import { Testimonials } from "./components/testimonials";
+
+import JsonData from "../../data/data.json";
+import SmoothScroll from "smooth-scroll";
+import { Navigation } from "../../components/Navigation";
+import { Team } from "../../components/Team";
+import { Contact } from "../../components/Contact";
+import { About } from "../../components/About";
+import { Header } from "../../components/Header";
+import { Departments} from "../../components/Dept";
+// import "./App.css";
+
+export const scroll = new SmoothScroll('a[href*="#"]', {
+  speed: 1000,
+  speedAsDuration: true,
+});
+
+const Home = () => {
+  const [landingPageData, setLandingPageData] = useState({});
+  useEffect(() => {
+    setLandingPageData(JsonData);
+  }, []);
+
+  return (
+    <div>
+      <Navigation />
+      <Header data={landingPageData.Header} />
+      {/* <Features data={landingPageData.Features} /> */}
+      {/* <Services data={landingPageData.Services} /> */}
+      <Departments data={landingPageData.Departments} />
+      <About data={landingPageData.About} />
+      {/* <Testimonials data={landingPageData.Testimonials} /> */}
+      <Team data={landingPageData.Team} />
+      <Contact data={landingPageData.Contact} />
+    </div>
   );
 };
+
+export default Home;
