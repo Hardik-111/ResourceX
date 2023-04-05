ec52898082542e5246a9a826ab2cc142cb6c2ca1
Ganesh Patidar <116238331+ganesh9dyv@users.noreply.github.com>
2023-04-06 01:33:59 +0530

    Update subject.js

--- backend/routes/subject.js
+++ backend/routes/subject.js
@@ -1,8 +1,10 @@
 import express  from "express";
-import { getMySubject} from "../controllers/subject.js";
+import { getMySubject,addAllSubject,addMySubject} from "../controllers/subject.js";
 import { isAuthenticated } from "../middlewares/auth.js";
 const router = express.Router();
 
+router.post("/new",isAuthenticated, addAllSubject );
+router.post("/all",isAuthenticated, addMySubject );
 router.get("/:id",isAuthenticated, getMySubject );
 
 
